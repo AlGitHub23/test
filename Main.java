adca5dd3de6b5816278d5591b8ae94a6f67a91b9
AlGitHub23 <122891951+AlGitHub23@users.noreply.github.com>
2023-01-19 01:08:27 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Arithmetic volue = new Arithmetic(6,10);
 
-        System.out.println("Сумма: " + volue.sum());
+        
         System.out.println("Произведение: " + volue.composition());
         System.out.println("Максимальное число: " + volue.max());
         System.out.println("Минимальное число: " + volue.min());
