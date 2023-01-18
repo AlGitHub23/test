6aebfe86054cb8191f3484c0ab3ff0417c01a9fd
AlGitHub23 <122891951+AlGitHub23@users.noreply.github.com>
2023-01-19 00:27:22 +0500

    create Arithmtic and Main

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Arithmetic volue = new Arithmetic(6,10);
+
+        System.out.println("Сумма: " + volue.sum());
+        System.out.println("Произведение: " + volue.composition());
+        System.out.println("Максимальное число: " + volue.max());
+        System.out.println("Минимальное число: " + volue.min());
+
+
+    }
+}
