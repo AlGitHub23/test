0ee2381a6d279fc184cea7b58c0a5959101d26a9
Alfiia Khasanova <alfonchik85@gmail.com>
2023-01-19 00:20:34 +0500

    one

---
+++ test/Arithmetic.java
@@ -0,0 +1,23 @@
+public class Arithmetic {
+    private int a;
+    private int b;
+    public Arithmetic(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+
+    public int sum() {
+        return a + b;
+    }
+    public int composition() {
+        return a * b;
+    }
+    public int max() {
+        return a > b ? a : b;
+    }
+
+    public int min() {
+        return a < b ? a : b;
+            }
+}
